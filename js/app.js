6236fad329818b8c43fdb9e685f636e5818ee476
alinukerotester <109532807+alinukerotester@users.noreply.github.com>
2022-09-11 20:36:32 +0300

    not sure ready

--- js/app.js
+++ js/app.js
@@ -14,25 +14,42 @@ startBtn.addEventListener("click", () => {
     recognitionService.continuous = true;
 
     recognitionService.onresult = handleResult;
-    recognitionService.start();
+
+
+    if (startBtn.classList.contains("btn-pulsating")) {
+        stopRecording();
+    } else {
+        startRecording();
+    }
 });
 
-function determineLanguage(){
-    const selected = document.querySelector('#language').value;
-    switch(selected){
-        case 'English':
+function determineLanguage() {
+    const selected = document.querySelector("#language").value;
+    switch (selected) {
+        case "English":
             return languages.English;
-        case 'Romanian':
+        case "Romanian":
             return languages.Romanian;
         default:
-            throw new Error('Language not supported');
+            throw new Error("Language not supported");
     }
 }
 
-function handleResult(event){
+function handleResult(event) {
     const results = [];
-    for(const result of event.results){
+    for (const result of event.results) {
         results.push(`${result[0].transcript}`)
     }
     textLog.innerHTML += results.at(-1);
+}
+
+function stopRecording() {
+    recognitionService.stop();
+    startBtn.classList.remove("btn-pulsating");
+    textLog.innerHTML += "<br />";
+}
+
+function startRecording() {
+    recognitionService.start();
+    startBtn.classList.add("btn-pulsating");
 }
